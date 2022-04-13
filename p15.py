985d0eca49313d31253fc68442b9fac8065e8370
Kevin <1348675040@qq.com>
2022-04-13 21:56:21 +0800

    new idea

---
+++ p15.py
@@ -0,0 +1,9 @@
+from urllib.request import urlopen
+from bs4 import  BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+bs =BeautifulSoup(html.read(), 'html.parser')
+namelist = bs.find_all(text='But tell me,')
+print(len(namelist))
+title = bs.find_all(id='redeviation-bs-sidebar')
+print(title)
